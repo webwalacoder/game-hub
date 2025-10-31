3608fa303cda8ee9ae0179ffa3e1af898d02de6f
webwalacoder <nalainahmad89@gmail.com>
2025-11-01 03:58:21 +0500

    Refactor- simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import type { GameQuery } from "@/App";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import type { Platform } from "./usePlatforms";
 import APIClient, { type FetchResponse } from "@/services/api-client";
+import ms from "ms";
 
 const apiClient = new APIClient<Game>("/games");
 
@@ -31,6 +32,6 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms("24h"),
   });
 export default useGames;
